66dfcd5a1490aaa458329299042e33db49a8b14f
Lukasz Samson <lukaszsamson@gmail.com>
2020-05-17 20:05:22 +0200

    edge conditions tests added

--- test/simple_test.erl
+++ test/simple_test.erl
@@ -78,8 +78,129 @@ max_sockets_test() ->
     end, Sockets),
     ok = erlzmq:term(C).
 
+max_sockets_no_limit_test() ->
+    {ok, C} = erlzmq:context(1),
+    Sockets = create_sockets(C, []),
+    ?assert(length(Sockets) > 0),
+    lists:foreach(fun (S) ->
+        ok = erlzmq:close(S)
+    end, Sockets),
+    ok = erlzmq:term(C).
+
 create_sockets(Context, List) ->
     case erlzmq:socket(Context, [req, {active, true}]) of
         {ok, S} -> create_sockets(Context, [S | List]);
         {error, emfile} -> List
     end.
+
+max_sockets_poll_limit_test_() ->
+    {timeout, 10, [
+        ?_assert(max_sockets_poll_limit() =:= ok)
+    ]}.
+
+max_sockets_poll_limit() ->
+    {ok, C} = erlzmq:context(1),
+    {ok, S1} = erlzmq:socket(C, [push, {active, false}]),
+    ok = erlzmq:setsockopt(S1, immediate, 1),
+    ok = erlzmq:setsockopt(S1, linger, 0),
+    ok = erlzmq:connect(S1, <<"tcp://127.0.0.1:9472">>),
+    
+    A = create_sends(S1),
+    ?assertEqual(emfile, A),
+    ok = erlzmq:close(S1),
+    ok = erlzmq:term(C),
+    ok.
+
+create_sends(S) ->
+    Self = self(),
+    spawn_link(fun () ->
+        case erlzmq:send(S, <<>>) of
+            {error, Error} ->
+                Self ! Error
+        end
+    end),
+    receive
+        emfile -> emfile
+    after
+        10 -> create_sends(S)
+    end.
+
+close_while_sending_test() ->
+    {ok, C} = erlzmq:context(),
+    {ok, S1} = erlzmq:socket(C, [req, {active, false}]),
+    spawn_link(fun () ->
+        {error, enotsock} = erlzmq:send(S1, <<>>)
+    end),
+    timer:sleep(1000),
+    ok = erlzmq:close(S1),
+    ok = erlzmq:term(C).
+
+close_while_receiving_test() ->
+    {ok, C} = erlzmq:context(),
+    {ok, S1} = erlzmq:socket(C, [rep, {active, false}]),
+    spawn_link(fun () ->
+        {error, enotsock} = erlzmq:recv(S1)
+    end),
+    timer:sleep(100),
+    ok = erlzmq:close(S1),
+    ok = erlzmq:term(C).
+
+term_while_sending_test() ->
+    {ok, C} = erlzmq:context(),
+    {ok, S1} = erlzmq:socket(C, [req, {active, false}]),
+    spawn_link(fun () ->
+        {error, eterm} = erlzmq:send(S1, <<>>)
+    end),
+    timer:sleep(1000),
+    spawn_link(fun () ->
+        timer:sleep(500),
+        ok = erlzmq:close(S1)
+    end),
+    ok = erlzmq:term(C).
+
+term_while_receiving_test() ->
+    {ok, C} = erlzmq:context(),
+    {ok, S1} = erlzmq:socket(C, [rep, {active, false}]),
+    spawn_link(fun () ->
+        {error, eterm} = erlzmq:recv(S1)
+    end),
+    timer:sleep(1000),
+    spawn_link(fun () ->
+        timer:sleep(500),
+        ok = erlzmq:close(S1)
+    end),
+    ok = erlzmq:term(C).
+
+close_while_receiving_active_test() ->
+    {ok, C} = erlzmq:context(),
+    {ok, S1} = erlzmq:socket(C, [rep, {active, true}]),
+    ok = erlzmq:bind(S1, <<"tcp://127.0.0.1:9472">>),
+    ok = erlzmq:close(S1),
+    ok = erlzmq:term(C),
+    receive
+        {zmq, S1, {error, enotsock}} -> ok
+    end.
+
+term_while_receiving_active_test() ->
+    {ok, C} = erlzmq:context(),
+    {ok, S1} = erlzmq:socket(C, [rep, {active, true}]),
+    ok = erlzmq:bind(S1, <<"tcp://127.0.0.1:9473">>),
+    spawn_link(fun () ->
+        timer:sleep(500),
+        ok = erlzmq:close(S1)
+    end),
+    ok = erlzmq:term(C),
+    receive
+        {zmq, S1, {error, eterm}} -> ok
+    end.
+
+close_while_terminating_test() ->
+    {ok, C} = erlzmq:context(1),
+    Sockets = create_sockets(C, []),
+    ?assert(length(Sockets) > 0),
+    lists:foreach(fun (S) ->
+        spawn_link(fun () ->
+            ok = erlzmq:close(S)
+        end)
+    end, Sockets),
+    ok = erlzmq:term(C).
